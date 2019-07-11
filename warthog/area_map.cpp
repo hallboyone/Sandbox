2ad07e1d2b6698a89a7c88dd84f9796f914ecc4f
Richard Hall <richard.hall@nrl.navy.mil>
2019-07-11 18:11:57 -0400

    Debuggin valgrind error

--- warthog/area_map.cpp
+++ warthog/area_map.cpp
@@ -1,5 +1,41 @@
 #include "area_map.h"
 
+
+bool area_map::inspectNeighbors(size_t row, size_t col, size_t range, bool state){
+  size_t range_row_low = range;
+  size_t range_row_high = range;
+  size_t range_col_low = range;
+  size_t range_col_high = range;
+  
+  //Make sure the range won't push us outside the boundries
+  if (row < range){//Negative row index
+    range_row_low = row;
+  }
+  if (pixel_height-1 < row + range){//To large row index
+    range_row_high = pixel_height - row-1;
+  }
+  if (col < range){//Negative col index
+    range_col_low = col;
+  }
+  if (pixel_width-1 < col + range){//To large col index
+    range_col_high = pixel_width - col-1;
+  }
+
+  for (size_t row_itr = range_row_low; row_itr<=range_row_high; row_itr++){
+    for (size_t col_itr = range_col_low; col_itr<=range_col_high; col_itr++){
+      if (row_itr != row || col_itr!=col){
+	if ((*bit_map[row_itr])[col_itr].is_black == state){
+	  return true;
+	}
+      }
+    }
+  }
+
+  return false;
+}
+      
+	
+    
 void area_map::read_bmp(std::ifstream & bmp){
       char file_type[2];
       char char_buf[8];
@@ -79,14 +115,25 @@ void area_map::read_bmp(std::ifstream & bmp){
       padding_size = row_size - pixel_width*bits_per_pixel/8;
      
       bit_map.resize(pixel_height); //Resize the bit_map vector to holds a pointer for each row
-  
+
+      int blue;
+      int green;
+      int red;
+      
       for (size_t i=0; i<pixel_height; i++){//Iterate through all the rows
 	bit_map[i] = new std::vector<pixel>;
 	(*bit_map[i]).resize(pixel_width);
 	for(size_t j=0; j<pixel_width; j++){//Iterate through all the pixels in a row
-	  (*bit_map[i])[j].blue = bmp.get();
-	  (*bit_map[i])[j].green = bmp.get();
-	  (*bit_map[i])[j].red = bmp.get();
+	  blue = bmp.get();
+	  green = bmp.get();
+	  red = bmp.get();
+	  (*bit_map[i])[j].id_building = 0;
+	  if (red < 10 && green < 10 && blue < 10){
+	    (*bit_map[i])[j].is_black = 1;
+	  }
+	  else{
+	    (*bit_map[i])[j].is_black = 0;
+	  }
 	}
 	bmp.ignore(padding_size);
       }
@@ -115,9 +162,17 @@ void area_map::read_bmp(std::ifstream & bmp){
   read_bmp(bmp);
   bmp.close();
   }
-
+/*
+  unsigned int area_map::groupBuildings(){
+    //Iterate through each pixel
+    //Check if pixel's row/col neighbors are part of a building group
+    //If at least one has a building id and all neighbors match, set pixels id to match
+    //If there are multiple id's in the pixel's neighbors, merge the id's and set the pixels id to match
+    for (size_t col = 0; col<pixel_height; col++){
+      for (size_t row = 0; row<pixel_width; row++){
+*/	
   area_map::~area_map(){
-    for(size_t i=0; i<pixel_height; i++){
-      delete(bit_map[i]);
+    for(std::vector<std::vector<pixel> * >::iterator it = bit_map.begin(); it != bit_map.end(); ++it){
+      delete(*it);
     }
   }
--- warthog/area_map.h
+++ warthog/area_map.h
@@ -9,29 +9,51 @@
 class area_map{
  private:
   typedef struct pixel_{
-    unsigned short int red;
-    unsigned short int green;
-    unsigned short int blue;
     bool is_black;
+    unsigned int id_building;
+    
   } pixel;
 
   //Holds the pixel structs. The std::vector<pixel>s are dynamically allocated
   std::vector<std::vector<pixel>* > bit_map;
 
+  /*
+  //Holds all the unique buildings as vectors of their indicies. 
+  std::vector<std::vector<std::pair<size_t, size_t> >* >
+  */
+  
   //The resolution of the image. 
   unsigned int pixel_vres;
   unsigned int pixel_hres;
   unsigned int pixel_width;
   unsigned int pixel_height;
+  unsigned int next_id;
   
   void read_bmp(std::ifstream & bmp);
-     
+  
   int char2Int(const char * c, int n);
 
+  //Looks in all neighbors within n spaces to see if any matches state
+  bool inspectNeighbors(size_t row, size_t col, size_t n, bool state);
+
+  /*
+  //Function to clean noise by setting all black pixels n spaces from a clear pixel clear
+  void trimNoise(size_t n);
+
+  //Switch all clear pixels within n spaces of a black pixel black
+  void addBuffer(size_t n);
+  */
  public:
   area_map();
 
   area_map(char * filename);
-
+  /*
+  //Function first trims noise and then adds buffer
+  void cleanBMP(size_t noise_level, size_t buffer_level){
+    trimNoise(noise_level);
+    addBuffer(buffer_level);
+    return
+      }
+  */
   ~area_map();
 };
