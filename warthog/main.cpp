1599c7c216c3fa7461b6778cb25b2a6368fed182
Richard Hall <richard.hall@nrl.navy.mil>
2019-07-23 17:36:28 -0400

    Debugging A-star alg

--- warthog/main.cpp
+++ warthog/main.cpp
@@ -1,7 +1,14 @@
 #include "area_map.h"
 
 int main(int argc, char** argv){
+  size_t x = 5;
+  size_t y = 3;
+  size_t x0 = 0;
+  size_t y0 = 0;
+  
   area_map map(argv[1], 4, false);
+  map.getDists(x, y);
+  map.printDir(x0, y0, x, y);
   //1) Deletes the current map_data grid (unless map_data==raw_data)
   //2) Creates a new grid from the raw data that trims noise and adds a buffer to the black objects
   //  map.clean(3, 5);
--- warthog/pixel_.cpp
+++ warthog/pixel_.cpp
@@ -1,5 +1,6 @@
 #include "area_map.h"
-#include <queue>
+
+#define SQ2 1.41421
 
 area_map::pixel_::pixel_(){
   neighbors[area_map::pixel_::dir::NW] = NULL;
@@ -12,7 +13,6 @@ area_map::pixel_::pixel_(){
   neighbors[area_map::pixel_::dir::W] = NULL;
 
   is_black = false;
-  id_building_ = 0;
 }
 
 area_map::pixel_::pixel_(bool is_black_){
@@ -26,15 +26,14 @@ area_map::pixel_::pixel_(bool is_black_){
   neighbors[area_map::pixel_::dir::W] = NULL;
 
   is_black = is_black_;
-  id_building_ = 0;
 }
 
 void area_map::pixel_::connectDiags(){
   //Recursivly call all the other pixels  
-  if(neighbors[area_map::pixel_::dir::N]!=NULL && x_coord==0){
+  if(neighbors[area_map::pixel_::dir::N]!=NULL && coord.first==0){
     neighbors[area_map::pixel_::dir::N]->connectDiags();
   }
-v  
+  
   if(neighbors[area_map::pixel_::dir::E]!=NULL){
     neighbors[area_map::pixel_::dir::E]->connectDiags();
   }
@@ -62,19 +61,154 @@ v
 }
 
 void area_map::pixel_::dist2Here(){
-  std::queue <pixel_ *> pix_q;
-
-  pix_q.push(neighbors[area_map::pixel_::dir::N]);
-  pix_q.push(neighbors[area_map::pixel_::dir::NE]);
-  pix_q.push(neighbors[area_map::pixel_::dir::E]);
-  pix_q.push(neighbors[area_map::pixel_::dir::SE]);
-  pix_q.push(neighbors[area_map::pixel_::dir::S]);
-  pix_q.push(neighbors[area_map::pixel_::dir::SW]);
-  pix_q.push(neighbors[area_map::pixel_::dir::W]);
-  pix_q.push(neighbors[area_map::pixel_::dir::NW]);
-
+  std::queue <area_map::pixel_ *> pix_q;
   
+  pix_dist[coord] = 0;
+
+  //Call on yourself
+  this->checkAround(pix_q, coord);
+  
+  while(pix_q.size() != 0){
+    (pix_q.front())->checkAround(pix_q, coord);
+    pix_q.pop();
+  }
 }
+
+void area_map::pixel_::checkAround(std::queue<area_map::pixel_ *> & pix_q, std::pair<size_t, size_t> & target){
+  pixel_* n  = neighbors[area_map::pixel_::dir::N];
+  pixel_* ne = neighbors[area_map::pixel_::dir::NE];
+  pixel_* e  = neighbors[area_map::pixel_::dir::E];
+  pixel_* se = neighbors[area_map::pixel_::dir::SE];
+  pixel_* s  = neighbors[area_map::pixel_::dir::S];
+  pixel_* sw = neighbors[area_map::pixel_::dir::SW];
+  pixel_* w  = neighbors[area_map::pixel_::dir::W];
+  pixel_* nw = neighbors[area_map::pixel_::dir::NW];
+  
+  //Is there another pixel up
+  if(n != NULL){
+    //Is the pixel NOT black
+    if(~n->is_black){
+      //Has the pixel already been inspected or is the last distance longer than it could be?
+      if (n->pix_dist.count(target)==0 || n->pix_dist[target] > pix_dist[target] + 1){
+	n->pix_dist[target] = pix_dist[target] + 1;
+	pix_q.push(n);
+      }
+    }
+  }
+
+  if(ne != NULL){
+    if(~ne->is_black){
+      if (ne->pix_dist.count(target)==0 || ne->pix_dist[target] > pix_dist[target] + SQ2){
+        ne->pix_dist[target] = pix_dist[target] + SQ2;
+	pix_q.push(ne);
+      }
+    }
+  }
+  
+  if(e!=NULL){
+    if(~e->is_black){
+      if (e->pix_dist.count(target)==0 || e->pix_dist[target] > pix_dist[target] + 1){
+        e->pix_dist[target] = pix_dist[target] + 1;
+	pix_q.push(e);
+      }
+    }
+  }
+  
+  if(se!=NULL){
+    if(~se->is_black){
+      if (se->pix_dist.count(target)==0 ||se->pix_dist[target] > pix_dist[target] + SQ2){
+        se->pix_dist[target] = pix_dist[target] + SQ2;
+	pix_q.push(se);
+      }
+    }
+  }
+  
+  if(s!=NULL){
+    if(~s->is_black){
+      if (s->pix_dist.count(target)==0 || s->pix_dist[target] > pix_dist[target] + 1){
+        s->pix_dist[target] = pix_dist[target] + 1;
+	pix_q.push(s);
+      }
+    }
+  }
+  
+  if(sw!=NULL){
+    if(~sw->is_black){
+      if (sw->pix_dist.count(target)==0 ||sw->pix_dist[target] > pix_dist[target] + SQ2){
+	sw->pix_dist[target] = pix_dist[target] + SQ2;
+	pix_q.push(sw);
+      }
+    }
+  }
+  
+  if(w!=NULL){
+    if(~w->is_black){
+      if (w->pix_dist.count(target)==0 ||w->pix_dist[target] > pix_dist[target] + 1){
+        w->pix_dist[target] = pix_dist[target] + 1;
+	pix_q.push(w);
+      }
+    }
+  }
+  if(nw!=NULL){
+    if(~nw->is_black){
+      if (nw->pix_dist.count(target)==0 || nw->pix_dist[target] > pix_dist[target] + SQ2){
+        nw->pix_dist[target] = pix_dist[target] + SQ2;
+	pix_q.push(nw);
+      }
+    }
+  }
+}
+
+void area_map::pixel_::initDists(size_t w, size_t h){
+  std::cout<<"("<<coord.first<<","<<coord.second<<")\n";
+ std::pair<size_t, size_t> cur_coord;
+  //Recursivly call all the other pixels  
+  if(neighbors[area_map::pixel_::dir::N] != NULL && coord.first==0){
+    neighbors[area_map::pixel_::dir::N]->initDists(w, h, cur_coord);
+  }
+  
+  if(neighbors[area_map::pixel_::dir::E]!=NULL){
+    neighbors[area_map::pixel_::dir::E]->initDists(w, h, cur_coord);
+  }
+  
+ 
+  for (size_t row = 0; row < h; row++){
+    //    std::cout<<row<<std::endl;
+    cur_coord.second = row;
+    for (size_t col = 0; col < w; col++){
+      cur_coord.first = col;
+      pix_dist[cur_coord] = w*h;
+    }
+  }
+  pix_dist[coord] = 0;
+  return;
+}
+
+
+void area_map::pixel_::initDists(size_t w, size_t h, std::pair<size_t, size_t> & cur_coord){
+  std::cout<<"("<<coord.first<<","<<coord.second<<")\n";
+  //Recursivly call all the other pixels  
+  if(neighbors[area_map::pixel_::dir::N] != NULL && coord.first==0){
+    neighbors[area_map::pixel_::dir::N]->initDists(w, h);
+  }
+  
+  if(neighbors[area_map::pixel_::dir::E]!=NULL){
+    neighbors[area_map::pixel_::dir::E]->initDists(w, h);
+  }
+  
+  //  std::pair<size_t, size_t> cur_coord;
+  for (size_t row = 0; row < h; row++){
+    //    std::cout<<row<<std::endl;
+    cur_coord.second = row;
+    for (size_t col = 0; col < w; col++){
+      cur_coord.first = col;
+      pix_dist[cur_coord] = w*h;
+    }
+  }
+  pix_dist[coord] = 0;
+  return;
+}
+
 //pixel_::pixel_(bool is_black, std::map<std::string, pixel_ *> neighbors);
 area_map::pixel_::~pixel_(){
   if (neighbors[area_map::pixel_::dir::W]==NULL){//If we are in the first col
