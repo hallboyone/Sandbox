86da3f9310d382034b58e917bd78fb34794223c1
RAH-Macbook Pro <rah75@duke.edu>
2020-01-03 12:09:02 -0500

    dif test

--- Machine_vision/image_manipulation.py
+++ Machine_vision/image_manipulation.py
@@ -1,12 +1,12 @@
 import cv2 as cv
 
 #Load the image of Einsein
-img_1 = cv.imread('camera_man_gray.png');
-img_2 = cv.imread('einstein_gray.png');
+img_1 = cv.imread('camera_man_gray.png', 0);
+img_2 = cv.imread('einstein_gray.png', 0);
 
 #Convert the image to grayscale
 #img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY);
 
-img_dif = (img_1-img_2) + (img_2 - img_1);
+img_dif = (img_1-img_2)*(img_1>img_2) + (img_2-img_1)*(img_1<=img_2);
 
 cv.imwrite('img_dif.png', img_dif)
