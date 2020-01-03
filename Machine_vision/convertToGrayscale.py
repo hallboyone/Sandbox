3ef18ab09825c8eb2704806452cd80f3938e033d
RAH-Macbook Pro <rah75@duke.edu>
2020-01-03 11:42:29 -0500

    Made the script to convert images to grayscale.

--- Machine_vision/convertToGrayscale.py
+++ Machine_vision/convertToGrayscale.py
@@ -5,10 +5,17 @@ filename = input("Please enter the filename : ")
 #Load the image of Einsein
 img = cv.imread(filename);
 
+#If the image could not be opened, prompt the user again
+while (img is None):
+    filename = input("Filename not found.\nPlease try again : ")
+    img = cv.imread(filename);
+
 #Convert the image to grayscale
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY);
 
-#Save the new grayscale image
+#Create the new filename
 [name, ext] = filename.split('.')
 new_name = name + "_gray." + ext
-cv.imwrtie(new_name, img_gray)
+
+#Save the new grayscale image
+cv.imwrite(new_name, img_gray)
