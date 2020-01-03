e3cfe41b192482c18c68c05362396c41018c6f2e
RAH-Macbook Pro <rah75@duke.edu>
2020-01-03 11:56:58 -0500

    test

--- Machine_vision/image_manipulation.py
+++ Machine_vision/image_manipulation.py
@@ -1,14 +1,12 @@
 import cv2 as cv
 
-filename = input("Please enter the filename : ")
-
 #Load the image of Einsein
-img = cv.imread(filename);
+img_1 = cv.imread('camera_man_gray.png');
+img_2 = cv.imread('einstein_gray.png');
 
 #Convert the image to grayscale
-img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY);
+#img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY);
 
-#Save the new grayscale image
-[name, ext] = filename.split('.')
-new_name = name + "_gray." + ext
-cv.imwrite(new_name, img_gray)
+img_dif = (img_1-img_2) + (img_2 - img_1);
+
+cv.imwrite('img_dif.png', img_dif)
