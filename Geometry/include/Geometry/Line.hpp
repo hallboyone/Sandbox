a2fdd832bb064537385f21262efeea85e31456b6
Richard Hall <rah75@duke.edu>
2020-05-28 16:15:57 -0400

    Adding functionallity to the Line class.

--- Geometry/include/Geometry/Line.hpp
+++ Geometry/include/Geometry/Line.hpp
@@ -3,44 +3,133 @@
 
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Geometry>
-#include <cmath>
+
 #include <iostream>
+#include <cmath>
+#include <iomanip> //setprecision
 
 #define COORD_TOL 10e-100
 
 namespace SharkPlot{
-  template <unsigned int DIM>
+  
+  /** Global typedef used to hold the diminsion*/
+  typedef unsigned int dim_t;
+
+  template <dim_t D> class Line;
+  template <dim_t D> std::ostream& operator<<(std::ostream & os, const Line<D>& ln);
+  
+  
+  /** A class representing a single, straight, directed line.
+   *
+   * The line consists of a \link #root_ root\endlink and a \link #tip_ tip\endlink
+   * which specify the start and end points of the line. Various transformations
+   * and operations can be carried out on the line accourding the the methods below.
+   * @tparam DIM A uint specifying the diminsion the line is in. 
+   */
+  template <dim_t D>
   class Line{
   private:
-    typedef Eigen::Matrix<double, DIM, 1> Coord;
+    /** Typedef of the Eigen Matrix specilization used as the #root_ and #tip_*/
+    typedef Eigen::Matrix<double, D, 1> Coord;
     
-    Eigen::Matrix<double, DIM, 1> root_;
-    Eigen::Matrix<double, DIM, 1> tip_;
+    /** Coordinate representing the starting point of the line*/
+    Eigen::Matrix<double, D, 1> root_;
+    /** Coordinate representing the ending point of the line*/
+    Eigen::Matrix<double, D, 1> tip_;
   public:
-    
+    /** @name Member access
+     *  Functions used to access the #root_ and #tip_ members 
+     */
+    ///@{
+    /** Returns a ref to the #root_*/
     Coord & root(){return root_;}
-    Coord & tip() {return tip_;}
-    void setRoot(const Coord & r){ root_ = r;}
-    void setTip (const Coord & t){ tip_ = t;}
+    /** Returns a ref to the #tip_*/
+    Coord & tip() {return  tip_;}
+    /** Returns a const ref to the #root_*/
+    const Coord & root()const {return root_;}
+    /** Returns a const ref to the #tip_*/
+    const Coord & tip() const {return  tip_;}
+    ///@}
     
-    double len(){return (root_ - tip_).norm();}
-    double norm(){return len();}
+    /** @name Member setting
+     *  Functions used to set the #root_ and #tip_ members 
+     */
+    ///@{
+    /** Updates the value of #root_ to match the passed in #Coord
+     * @param r #Coord object that #root_ will be set to 
+     **/
+    void setRoot(const Coord & r){ root_ = r;}
+    /** Updates the value of #tip_ to match the passed in #Coord
+     * @param t #Coord object that #tip_ will be set to 
+     **/
+    void setTip (const Coord & t){ tip_ = t;}
+    ///@}
 
-    void scale(double n, bool hold_root_ = false){
+    /** @name Norm operations
+     * Methods that implement various norm operations on the line
+     */
+    ///@{
+    /** Return the line's 2-norm */
+    double len() const {return (root_ - tip_).norm();}
+    /** Return the line's 2-norm */
+    double norm() const {return len();}
+    /** Return the line's inf-norm */
+    template <int p>
+    double lpNorm() const {
+      Coord diff = root_-tip_;
+      return diff.template lpNorm<p>();}
+    ///@}
+
+    /** @name Transformations
+     * Methods implementing various transformations on the object
+     */
+    ///@{
+    /** Scales the line by n
+     * @param n Amount to scale the line by. If very small, a warning is printed.
+     * @param lock_root If set to true, the line is scaled by only moving the #tip_.
+     * Else, the #root_ and #tip_ are scaled evenly.
+     */
+    void scale(double n, bool lock_root = false){
       if (fabs(n) < COORD_TOL)
-	std::cerr<<"Warning: Scaling line by very small value. May lead to numerical errors.\n";
-      if (hold_root_){//Scale without shifting the root
-	tip_ = root_ + n*(tip_ - root_);
-      }
+	std::cerr<<"Warning: Scaling line by very small value. "
+		 <<"May lead to numerical errors.\n";
+      if (lock_root) tip_ = root_ + n*(tip_ - root_);
       else{
 	root_ *= n;
 	tip_  *= n;
       }
     }
-    
-    void normalize(bool lock_root_ = false){scale(1/len(), lock_root_);}
-      
+    /** Scales to line so that its 2-norm is 1
+     * @param lock_root If set to true, the line is scaled by only moving the #tip_.
+     * Else, the #root_ and #tip_ are scaled evenly.
+     */
+    void normalize(bool lock_root = false){scale(1/len(), lock_root);}
+    ///@}
+
+    friend std::ostream& operator<< <D>(std::ostream& os, const Line<D>& ln);
   };
+
+  
+  template <dim_t D>
+  std::ostream & operator<<(std::ostream & os, const Line<D>& ln){
+    if (D > 3){
+      os<<"[ln: Dim="<<D<<": len="<<ln.len()<<"]\n";
+    }
+    else {
+      os<<std::setprecision(5)<<"[";
+      for(dim_t i = 0; i<D; i++){
+	os<<ln.root()[i];
+	if (i<D-1) os<<",";
+      }
+      os<<"]--[";
+      for(dim_t i = 0; i<D; i++){
+	os<<ln.tip()[i];
+	if (i<D-1) os<<",";
+      }
+      os<<"]"<<std::setprecision(10);
+    }
+    return os;
+    }
 }//Sharkplot
-    
+
 #endif //LINE_HPP
--- Geometry/src/Geometry/main.cpp
+++ Geometry/src/Geometry/main.cpp
@@ -6,19 +6,30 @@ int main()
 {
   SharkPlot::Line<3> l1;
   l1.root() << 1, 3, -5;
-  std::cout<<l1.root()<<std::endl;
-  std::cout<<l1.len()<<std::endl;
+  //std::cout<<l1.root()<<std::endl;
+  //std::cout<<l1.len()<<std::endl;
   l1.normalize();
-  std::cout<<l1.len()<<std::endl;
-  std::cout<<l1.root()<<std::endl<<std::endl;
+  //std::cout<<l1.len()<<std::endl;
+  //std::cout<<l1.root()<<std::endl<<std::endl;
   l1.setRoot(Eigen::Vector3d(1,0,0));
   l1.setTip(Eigen::Vector3d(1,0,2));
   l1.normalize();
   
-  std::cout<<l1.root()<<"|-|"<<l1.tip()<<std::endl<<std::endl;
+  //std::cout<<l1.root()<<"|-|"<<l1.tip()<<std::endl<<std::endl;
   l1.setRoot(Eigen::Vector3d(1,0,0));
   l1.setTip(Eigen::Vector3d(1,0,2));
-  std::cout<<l1.root()<<"|-|"<<l1.tip()<<std::endl<<std::endl;
+  //std::cout<<l1.root()<<"|-|"<<l1.tip()<<std::endl<<std::endl;
   l1.normalize(true);
-  std::cout<<l1.root()<<"|-|"<<l1.tip()<<std::endl;
+  //std::cout<<l1.root()<<"|-|"<<l1.tip()<<std::endl;
+  std::cout<<l1<<std::endl;
+
+  SharkPlot::Line<4> l2;
+  l2.root() << 0,0,0,0;
+  l2.tip() << 1,1,1,1;
+  std::cout<<l2<<std::endl;
+
+  SharkPlot::Line<3> l3(l1);
+  l3.tip() << 1, 2, 3;
+  std::cout<<l1<<std::endl<<l3<<std::endl;
+  std::cout<<l3.lpNorm<1>()<<" "<<l3.lpNorm<Eigen::Infinity>()<<std::endl;
 }
