bee1532fd236b950821945a0a0b0b59541a033e6
Richard Hall <rah75@duke.edu>
2020-08-09 23:04:53 -0400

    Added an erase function to the MergeSet. It may be getting close to being done.

--- MergeSet/MergeSet.hpp
+++ MergeSet/MergeSet.hpp
@@ -1,36 +1,73 @@
-#ifndef MERGESET
-#define MERGESET
+#ifndef MERGE_SET
+#define MERGE_SET
 
 template <class MOO>
 class MergeSet{
 public:
+  /**
+   * A single node of the BST containing a single instance of a MOO object and 
+   * the left and right pointers.
+   **/
   class MON{
   public:
-    MOO data_;
-    MON * r_ptr_;
-    MON * l_ptr_;
-    MON(const MOO & data): data_(data), r_ptr_(NULL), l_ptr_(NULL){}
+    MOO data;
+    MON * right;
+    MON * left;
+
+    MON(const MOO & d): data(d), right(NULL), left(NULL){}
   };
 
   MergeSet(): root_(NULL), sz_(0){}
   
   //Add MOO_t to the bst using recursion. Merge if duplicate found
   MOO * insert(const MOO & new_el){
-    MON ** add_here = &root_;
-    while (*add_here != NULL){
-      if (new_el == (*add_here)->data_) {
-	(*add_here)->data_.merge(new_el);
-	return &((*add_here)->data_);
-      }
-      else if (new_el < (*add_here)->data_) add_here = &(*add_here)->l_ptr_;
-      else add_here = &(*add_here)->r_ptr_;
+    MON ** add_here = findNode(new_el); // &root_;
+    if (*add_here != NULL) {
+      (*add_here)->data.merge(new_el);
+      return &((*add_here)->data);
     }
-    
-    (*add_here) = new MON(new_el);
-    sz_++;
-    return &((*add_here)->data_);
+    else {
+      (*add_here) = new MON(new_el);
+      sz_++;
+      return &((*add_here)->data);
+    }
   }
 
+  void erase(const MOO & el){
+    MON ** upstream_pp = findNode(el);
+    MON *  node_p  = *upstream_pp;
+    if (node_p == NULL) return;
+
+    if (node_p->left != NULL) {
+      MON** sub_node_pp = &(node_p->left);
+      while ((*sub_node_pp)->right != NULL){
+	sub_node_pp = &((*sub_node_pp)->right);
+      }
+
+      *upstream_pp = *sub_node_pp;
+      *sub_node_pp = (*sub_node_pp)->left;
+      (*upstream_pp)->left = node_p->left;
+      (*upstream_pp)->right = node_p->right;
+    }
+    else {
+      *upstream_pp = node_p->right;
+    }
+
+    delete node_p;
+    sz_--;
+  }
+
+  MOO * find(const MOO & el){
+    return &((findNode(el))->data);
+  }
+
+  unsigned int size(){ return sz_; }
+
+  void clear(){
+    destroy(root_);
+    sz_ = 0;
+  }
+  
   void printSet(){
     std::cout<<"MergeSet with "<<sz_<<" elements.\n";
     printNodes(root_);
@@ -47,20 +84,39 @@ private:
 
   void printNodes(MON * node){
     if (node != NULL){
-      printNodes(node->l_ptr_);
-      node->data_.print();
-      printNodes(node->r_ptr_);
+      printNodes(node->left);
+      node->data.print();
+      printNodes(node->right);
     }
   }
   
   void destroy(MON * node){
     if (node != NULL){
-      destroy(node->l_ptr_);
-      destroy(node->r_ptr_);
+      destroy(node->left);
+      destroy(node->right);
       delete node;
     }
     return;
   }
 
+  /**
+   * Iterate through BST looking for el. Return pointer to pointer to matching
+   * node or pointer to NULL pointer if not found
+   **/
+  MON ** findNode(const MOO & el){
+    MON ** search_ptr = &root_;
+    while (*search_ptr != NULL){
+      if (el == (*search_ptr)->data) {
+        break;
+      }
+      else if (el < (*search_ptr)->data) {
+	search_ptr = &(*search_ptr)->left;
+      }
+      else {
+	search_ptr = &(*search_ptr)->right;
+      }
+    }
+    return search_ptr;
+  }
 };
 #endif
--- MergeSet/main.cpp
+++ MergeSet/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 int main(){
+  /*
   MooType var1("This", -10);
   MooType var2(" is", -10);
   MooType var3(" a test", -10);
@@ -12,7 +13,7 @@ int main(){
   MooType var6(" it will", 1);
   MooType var7("But we will", 2);
   MooType var8(" have to see", 2);
-  /*
+  
   std::cout<<"var1 < var2 = "<<var1.compare(var2)<<std::endl;
   std::cout<<"var1 < var3 = "<<var1.compare(var3)<<std::endl;
   std::cout<<"var1 < var4 = "<<var1.compare(var4)<<std::endl;
@@ -35,16 +36,83 @@ int main(){
   //  var1.merge(var4);
   */
 
+  MooType v1("1", 64);
+  MooType v2("2", 32);
+  MooType v3("3", 96);
+  MooType v4("4", 16);
+  MooType v5("5", 48);
+  MooType v6("6", 80);
+  MooType v7("7", 112);
+  MooType v8("8", 8);
+  MooType v9("9", 24);
+  MooType v10("10", 40);
+  MooType v11("11", 56);
+  MooType v12("12", 72);
+  MooType v13("13", 88);
+  MooType v14("14", 104);
+  MooType v15("15", 120);
+  MooType v16("16", 28);
+  MooType v17("17", 52);
+  MooType v18("18", 60);
+  MooType v19("19", 100);
+  MooType v20("20", 108);
+  MooType v21("21", 124);
+  MooType v22("22", 26);
+  MooType v23("23", 30);
+  MooType v24("24", 62);
+  MooType v25("25", 98);
+  MooType v26("26", 102);
+  MooType v27("27", 106);
+  MooType v28("28", 110);
+  MooType v29("29", 126);
+  MooType v30("30", 25);
+  MooType v31("31", 27);
+  MooType v32("32", 31);
+  MooType v33("33", 61);
+  MooType v34("34", 63);
+  MooType v35("35", 105);
+  MooType v36("36", 125);
+  MooType v37("37", 127);
+  
   MergeSet<MooType> var;
-  var.insert(var1);
-  var.insert(var2);
-  var.insert(var7);
-  var.insert(var5);
-  var.insert(var3);
-  var.insert(var4);
-  var.insert(var6);
-  var.insert(var8);
-
+  var.insert(v1);
+  var.insert(v2);
+  var.insert(v3);
+  var.insert(v4);
+  var.insert(v5);
+  var.insert(v6);
+  var.insert(v7);
+  var.insert(v8);
+  var.insert(v9);
+  var.insert(v10);
+  var.insert(v11);
+  var.insert(v12);
+  var.insert(v13);
+  var.insert(v14);
+  var.insert(v15);
+  var.insert(v16);
+  var.insert(v17);
+  var.insert(v18);
+  var.insert(v19);
+  var.insert(v20);
+  var.insert(v21);
+  var.insert(v22);
+  var.insert(v23);
+  var.insert(v24);
+  var.insert(v25);
+  var.insert(v26);
+  var.insert(v27);
+  var.insert(v28);
+  var.insert(v29);
+  var.insert(v30);
+  var.insert(v31);
+  var.insert(v32);
+  var.insert(v33);
+  var.insert(v34);
+  var.insert(v35);
+  var.insert(v36);
+  var.insert(v37);
+  var.erase(v1);
   var.printSet();
   return 1;
 }
