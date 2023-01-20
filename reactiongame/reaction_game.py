e571959dcb86670d2714a7de92e9337a2cd480e3
Richard Hall <rah75@duke.edu>
2023-01-20 15:53:44 -0500

    Improved game and added start script.

--- reactiongame/reaction_game.py
+++ reactiongame/reaction_game.py
@@ -11,26 +11,29 @@ def type_text(msg, typing_speed):
         sys.stdout.flush()
         time.sleep(random.random()*10.0/typing_speed)
 
+difficulty_scaler = 1
+if len(sys.argv) > 1:
+    difficulty_scaler = float(sys.argv[1])
+
 hack_chars = ['1', 'z', '0', 'm']
 type_speed = [500, 1000, 1500, 2000, 2500, 3000, 3500]
 min_char_num = [80, 160, 240, 400, 500, 700, 800]
 max_char_num = [400, 800, 1200, 2000, 2500, 3250, 4000]
 wait_time = [2, 1.75, 1.75, 1.5, 1.25, 1.125, 1]
 rand_char_set = string.ascii_uppercase + string.ascii_lowercase + "\n"
-terminal_string = Fore.GREEN + "\033[1msecurity@GLBC-Mainfraim" + Style.RESET_ALL + ":\033[1m" + Fore.BLUE + "~ $ " + Style.RESET_ALL
-
+term_str= Fore.GREEN+"\033[1msecurity@GLBC-Mainfraim"+Style.RESET_ALL+":\033[1m"+Fore.BLUE+"~ $ "+Style.RESET_ALL
 
 os.system('cls' if os.name == 'nt' else 'clear')
 while True:
     while True:
-        usr_input = input(terminal_string)
+        usr_input = input(term_str)
         if usr_input.endswith("exit"):
             exit()
         elif usr_input.endswith("sys.start"):
             break
         print("Unknown command. Type sys.start to begin.")
 
-    print(terminal_string, end="")
+    print(term_str, end="")
     sys.stdout.flush()
     time.sleep(0.5)
     type_text("sys.transfer surveillance all 2h \n", 100)
@@ -38,37 +41,48 @@ while True:
     print("Retrieving...")
     time.sleep(0.5)
     print(Fore.BLACK + Back.RED + "ACCESS DENIED - Unauthorized User" + Style.RESET_ALL)
+    
+    # Override 7 times
     for j in range(7):
-        type_text(''.join(random.choices(rand_char_set, k=random.randint(min_char_num[j], max_char_num[j]))), type_speed[j])
-        
-        hack_char = hack_chars[random.randint(0,3)]
+        # Generate random amounts of random text
+        num_chars = random.randint(min_char_num[j], max_char_num[j])
+        type_text(''.join(random.choices(rand_char_set, k=num_chars)), type_speed[j])
 
+        # Get override variables
+        hack_char = hack_chars[random.randint(0,3)]
+        right_key_pressed = False
+        wrong_key_pressed = False
+        
+        # Print override message to terminal
         print(Fore.RED + "\nOverride security "+ str(j+1) + "/7 (press " + hack_char + ")" + Style.RESET_ALL + " ", end="")
         sys.stdout.flush()
 
+        # Wait for user input
         start_time = time.time()
-        key_pressed_fast_enough = False
-        wrong_key_pressed = False
-        while time.time() - start_time < wait_time[j]:
+        while time.time() - start_time < difficulty_scaler * wait_time[j]:
             time.sleep(0.01)
-            if wrong_key_pressed:
+            if wrong_key_pressed or right_key_pressed:
+                # Keystroke sensed. No need to wait longer
                 break
+            
+            # Look for pressed keys.
             for c in string.ascii_lowercase + string.digits:
                 if keyboard.is_pressed(c):
                     if c==hack_char:
                         print(Fore.BLACK + Back.GREEN + "override successfull" + Style.RESET_ALL)
-                        time.sleep(1)
-                        key_pressed_fast_enough = True
+                        right_key_pressed = True
                     else:
                         print(Fore.BLACK + Back.RED+ "invalid override key\n" + Style.RESET_ALL)
                         wrong_key_pressed = True
                     break
                 
-        if not key_pressed_fast_enough:
+        # Right key not pressed fast enough. Game over, restart for loop
+        if not right_key_pressed:
             print(Fore.BLACK + Back.RED + "Threat eleminated. System locked." + Style.RESET_ALL)
             break
 
-    if key_pressed_fast_enough:
+    # For loop ended with right key pressed ==> Game is won!
+    if right_key_pressed:
         print(Fore.GREEN + "Download complete. Ending process..." + Style.RESET_ALL)
 
-    time.sleep(0.5)
+    time.sleep(2)
---
+++ reactiongame/start_game.sh
@@ -0,0 +1,1 @@
+sudo ~/dev/Sandbox/reactiongame/.venv/bin/python ~/dev/Sandbox/reactiongame/reaction_game.py $1
